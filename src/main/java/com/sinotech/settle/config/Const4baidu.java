7666cbd581fe629c1328d384541b1231daa3b21c
wujh88 <wjh071@gmail.com>
2018-08-25 15:42:19 +0800

    整理更新

---
+++ src/main/java/com/sinotech/settle/config/Const4baidu.java
@@ -0,0 +1,7 @@
+package com.sinotech.settle.config;
+
+public class Const4baidu {
+    public static final String BAIDU_AI_APPID = "eee";
+    public static final String BAIDU_AI_APPKEY = "erererer";
+    public static final String BAIDU_AI_SECRETKEY = "ttrggfdxsdd";
+}
--- src/main/java/com/sinotech/settle/demo/service/impl/Text2audioServiceImpl.java
+++ src/main/java/com/sinotech/settle/demo/service/impl/Text2audioServiceImpl.java
@@ -3,6 +3,7 @@ package com.sinotech.settle.demo.service.impl;
 import com.baidu.aip.speech.AipSpeech;
 import com.baidu.aip.speech.TtsResponse;
 import com.baidu.aip.util.Util;
+import com.sinotech.settle.config.Const4baidu;
 import com.sinotech.settle.demo.service.Text2audioService;
 import org.json.JSONObject;
 import org.springframework.stereotype.Service;
@@ -12,13 +13,9 @@ import java.io.IOException;
 @Service
 public class Text2audioServiceImpl implements Text2audioService {
 
-    private final String appId = "11680458";
-    private final String appKey = "n38pBi95TsZVVDKe8izTbqDc";
-    private final String secretKey = "5kugCPczckCi7luy7K58d9FkkjpcCYZA";
-
     @Override
     public void text2audio(String text) {
-        AipSpeech client = new AipSpeech(appId, appKey, secretKey);
+        AipSpeech client = new AipSpeech(Const4baidu.BAIDU_AI_APPID, Const4baidu.BAIDU_AI_APPKEY, Const4baidu.BAIDU_AI_SECRETKEY);
         TtsResponse res = client.synthesis(text, "zh", 1, null);
         byte[] data = res.getData();
         JSONObject res1 = res.getResult();
