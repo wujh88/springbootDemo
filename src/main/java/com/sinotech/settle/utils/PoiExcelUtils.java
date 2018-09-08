b7059e1b033e92bc5aa2b9a6c973e383a5afd82e
wujh88 <wjh071@gmail.com>
2018-09-08 11:03:43 +0800

    修复更换jar包后方法引用错误

--- src/main/java/com/sinotech/settle/utils/PoiExcelUtils.java
+++ src/main/java/com/sinotech/settle/utils/PoiExcelUtils.java
@@ -1,6 +1,7 @@
 package com.sinotech.settle.utils;
 
-import org.apache.commons.collections.CollectionUtils;
+//import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.poi.hssf.usermodel.*;
